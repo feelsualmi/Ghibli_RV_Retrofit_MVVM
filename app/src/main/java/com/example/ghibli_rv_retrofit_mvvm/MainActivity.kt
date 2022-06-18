c96139b695b86127d8ae8abad6f015dc489400f7
Gleb Tarasov <tge_97@mail.ru>
2022-06-18 08:06:37 +0300

    v0.1

--- app/src/main/java/com/example/ghibli_rv_retrofit_mvvm/MainActivity.kt
+++ app/src/main/java/com/example/ghibli_rv_retrofit_mvvm/MainActivity.kt
@@ -2,14 +2,11 @@ package com.example.ghibli_rv_retrofit_mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ghibli_rv_retrofit_mvvm.databinding.ActivityMainBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
